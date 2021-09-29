a05e5b2dcf6cca9cced7fc78ca79890a9e55fc7b
Anton <Chipazawra@mail.ru>
2021-09-29 11:30:18 +0300

    refactor ide hints

--- go.mod
+++ go.mod
@@ -2,8 +2,8 @@ module github.com/Chipazawra/MyleetCodeProblemSolves
 
 go 1.17
 
-replace(
-    github.com/Chipazawra/MyleetCodeProblemSolves/problems/validParentheses => "./problems/validParentheses"
-    github.com/Chipazawra/MyleetCodeProblemSolves/problems/numberOfIslands => "./problems/numberOfIslands"
-    github.com/Chipazawra/MyleetCodeProblemSolves/problems/mergeIntervals => "./problems/mergeIntervals" 
-)
+// replace(
+//     github.com/Chipazawra/MyleetCodeProblemSolves/problems/validParentheses => "./problems/validParentheses"
+//     github.com/Chipazawra/MyleetCodeProblemSolves/problems/numberOfIslands => "./problems/numberOfIslands"
+//     github.com/Chipazawra/MyleetCodeProblemSolves/problems/mergeIntervals => "./problems/mergeIntervals" 
+// )
--- main.go
+++ main.go
@@ -17,36 +17,36 @@ func main() {
 	fmt.Println(validParentheses.IsValid("()[]{}"))
 
 	fmt.Println(numberOfIslands.NumberOfIslands([][]byte{
-		[]byte{'1', '1', '1', '1', '0'},
-		[]byte{'1', '1', '0', '1', '0'},
-		[]byte{'1', '1', '0', '0', '0'},
-		[]byte{'0', '0', '0', '0', '0'},
+		{'1', '1', '1', '1', '0'},
+		{'1', '1', '0', '1', '0'},
+		{'1', '1', '0', '0', '0'},
+		{'0', '0', '0', '0', '0'},
 	}))
 
 	fmt.Println(numberOfIslands.NumberOfIslands([][]byte{
-		[]byte{'1', '1', '0', '0', '0'},
-		[]byte{'1', '1', '0', '0', '0'},
-		[]byte{'0', '0', '1', '0', '0'},
-		[]byte{'0', '0', '0', '1', '1'},
+		{'1', '1', '0', '0', '0'},
+		{'1', '1', '0', '0', '0'},
+		{'0', '0', '1', '0', '0'},
+		{'0', '0', '0', '1', '1'},
 	}))
 
 	fmt.Println(mergeintervals.Merge([][]int{
-		[]int{1, 3},
-		[]int{2, 6},
-		[]int{8, 10},
-		[]int{15, 18},
+		{1, 3},
+		{2, 6},
+		{8, 10},
+		{15, 18},
 	}))
 	fmt.Println(mergeintervals.Merge([][]int{
-		[]int{1, 4},
-		[]int{4, 5},
+		{1, 4},
+		{4, 5},
 	}))
 	fmt.Println(mergeintervals.Merge([][]int{
-		[]int{1, 4},
-		[]int{0, 4},
+		{1, 4},
+		{0, 4},
 	}))
 	fmt.Println(mergeintervals.Merge([][]int{
-		[]int{0, 0},
-		[]int{1, 4},
+		{0, 0},
+		{1, 4},
 	}))
 
 }
